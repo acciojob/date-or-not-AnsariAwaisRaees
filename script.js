ec00d54a641aa6fa674cca4dfd10aa1ea7080091
Awais Raees Ansari <80111573+AnsariAwaisRaees@users.noreply.github.com>
2023-11-28 14:18:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 var isDate = function (input) {
-  //   write your code here
-};
+	//   write your code here
+	if (Object.prototype.toString.call(input) === '[object Date]') {
+		return !isNaN(input.getTime());
+	}
+
+	if(typeof input === 'string'){
+		const parsedDate = new Date(input);
+		return !isNaN(parsedDate.getTime());
+	}
+
+	return false;
+}; 
 
 // Do not change the code below.
 const input = prompt("Enter Date.");
